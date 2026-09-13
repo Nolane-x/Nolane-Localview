c581290c814f41059626e62686bf9a230da72316
Nolane-x <casioreview20@gmail.com>
2026-09-13 21:03:17 +0700

    feat(v43): export M06 application-incarnation authority

--- crates/macos-ax-provider/src/provider.rs
+++ crates/macos-ax-provider/src/provider.rs
@@ -1,8 +1,12 @@
 #[path = "lib.rs"]
 mod permission;
+mod application_incarnation;
 mod element_binding;
+mod observer_application;
 mod observer_reliability;
 
+pub use application_incarnation::*;
 pub use element_binding::*;
+pub use observer_application::*;
 pub use observer_reliability::*;
 pub use permission::*;
